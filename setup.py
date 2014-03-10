517b405544f91117c2f7119242b1095819de0ea3
Tim Bielawa <tbielawa@redhat.com>
2014-03-10 18:26:41 -0400

    Python setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='bitmath',
+      version='0.5.0',
+      description='Pythonic module for representing and manipulating file sizes with different prefix notations.',
+      maintainer='Tim Bielawa',
+      maintainer_email='timbielawa@gmail.com',
+      url='https://github.com/tbielawa/bitmath',
+      license='MIT',
+      package_dir={'bitmath': 'bitmath'},
+      packages=['bitmath']
+)
